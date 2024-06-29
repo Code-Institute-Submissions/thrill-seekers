1b325be5b7525fd9357220c3d132db09fb2133ed
Stephen Sure <stephan.sure@gmail.com>
2024-06-29 15:50:58 +0000

    Change css sign in form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -125,7 +125,7 @@ const SignUpForm = () => {
       </Col>
       <Col
         md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignUpColumn}`}
       >
         <Image
           className={`${appStyles.FillerImage}`}
