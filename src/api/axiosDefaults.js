e51b34bd8295c5f0f9a6cb9535c906dcdcfed21c
Stephen Sure <stephan.sure@gmail.com>
2024-06-30 12:45:56 +0000

    Change axios defaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://thrill-seekers-api-5fd87044d4ac.herokuapp.com/";
+axios.defaults.baseURL = "https://thrill-seekers-api-5fd87044d4ac.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
