0f63904d3622b46ca513292ab470face284c2355
Stephen Sure <stephan.sure@gmail.com>
2024-07-03 14:14:31 +0000

    Change hero image

--- src/pages/parks/Home.js
+++ src/pages/parks/Home.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
-import bgimage from "../../assets/images/coaster-hero-pexels.webp";
-
-
+import styles from '../../styles/Home.module.css';
+import bgImage from '../../assets/images/coaster-hero-pexels.webp';
 
 const Home = () => {
     return (
-        <div id="CustomJumbotron">
-            <Jumbotron style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover' }}> 
-                <h1>Welcome to ThrillSeeker</h1>
-                <h2>The Ultimate Guide to Adventure</h2>
-            </Jumbotron>
-        </div>
-
+        <>
+            <div className={styles.heroSection} style={{backgroundImage: `url(${bgImage})`}}>
+                <div className={styles.heroContent}>
+                    <h1>Welcome to ThrillSeeker</h1>
+                    <h2>The Ultimate Guide to Adventure</h2>
+                </div>
+            </div>
+            <div className="Main">
+                {/* Rest of your content goes here */}
+            </div>
+        </>
     );
 };
-
 export default Home;
