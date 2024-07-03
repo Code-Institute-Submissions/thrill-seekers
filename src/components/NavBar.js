1c6e5475d475d76fe44da85b13c67eb362b7a59b
Stephen Sure <stephan.sure@gmail.com>
2024-07-03 17:40:31 +0000

    Add media querie for hero image

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Container, Nav, Dropdown} from "react-bootstrap";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo2.webp";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import {
