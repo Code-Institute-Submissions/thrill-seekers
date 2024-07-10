408eefdefa154fde2fa234619f8f0e6e92717a08
Stephen Sure <stephan.sure@gmail.com>
2024-07-10 12:46:56 +0000

    Add searchbar

--- src/pages/parks/ParksPage.js
+++ src/pages/parks/ParksPage.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Form, Col } from "react-bootstrap";
 import Park from "./Park";
 import Asset from "../../components/Asset";
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
+
+
 import styles from "../../styles/Park.module.css";
 
 function ParksPage({ message, filter = "" }) {
@@ -12,10 +14,12 @@ function ParksPage({ message, filter = "" }) {
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchParks = async () => {
       try {
-        const { data } = await axiosReq.get(`/parks/?${filter}`);
+        const { data } = await axiosReq.get(`/parks/?${filter}search=${query}`);
         setParks(data);
         setHasLoaded(true);
       } catch (err) {
@@ -24,12 +28,34 @@ function ParksPage({ message, filter = "" }) {
     };
 
     setHasLoaded(false);
-    fetchParks();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchParks();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
 
 
   return (
     <Row>
+      <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search parks"
+          />
+        </Form>
+
+
+
       {hasLoaded ? (
         parks.results.length ? (
           parks.results.map((park) => (
