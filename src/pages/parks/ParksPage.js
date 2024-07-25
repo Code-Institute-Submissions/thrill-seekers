39b0aa8aa54f8c767b07f02b3f48967e30924703
Stephen Sure <stephan.sure@gmail.com>
2024-07-25 18:36:26 +0000

    Add form lable to search bar

--- src/pages/parks/ParksPage.js
+++ src/pages/parks/ParksPage.js
@@ -40,48 +40,50 @@ function ParksPage({ message, filter = "" }) {
 
   return (
     <Container className={styles.ParkContainer}>
-    <Row>
-      <Col xs={12} className={styles.SearchBarContainer}>
-        <Form
-          className={styles.SearchBar}
-          onSubmit={(event) => event.preventDefault()}
-        >
-          <i className={`fas fa-search ${styles.SearchIcon}`} />
-          <Form.Control
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-            type="text"
-            className="mr-sm-2"
-            placeholder="Search parks"
-          />
-        </Form>
-      </Col>
-
-      {hasLoaded ? (
-        parks.results.length ? (
-          <InfiniteScroll
-            dataLength={parks.results.length}
-            next={() => fetchMoreData(parks, setParks)}
-            hasMore={!!parks.next}
-            loader={<Asset spinner />}
+      <Row>
+        <Col xs={12} className={styles.SearchBarContainer}>
+          <Form
+            className={styles.SearchBar}
+            onSubmit={(event) => event.preventDefault()}
           >
-            {parks.results.map((park) => (
-              <Col key={park.id} xs={12} className="mb-3">
-                <Park {...park} setParks={setParks} />
-              </Col>
-            ))}
-          </InfiniteScroll>
+            <label htmlFor="search" className="sr-only">Search parks</label>
+            <i className={`fas fa-search ${styles.SearchIcon}`} />
+            <Form.Control
+              id="search"
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              type="text"
+              className="mr-sm-2"
+              placeholder="Search parks"
+            />
+          </Form>
+        </Col>
+
+        {hasLoaded ? (
+          parks.results.length ? (
+            <InfiniteScroll
+              dataLength={parks.results.length}
+              next={() => fetchMoreData(parks, setParks)}
+              hasMore={!!parks.next}
+              loader={<Asset spinner />}
+            >
+              {parks.results.map((park) => (
+                <Col key={park.id} xs={12} className="mb-3">
+                  <Park {...park} setParks={setParks} />
+                </Col>
+              ))}
+            </InfiniteScroll>
+          ) : (
+            <Col xs={12}>
+              <Asset src={NoResults} message={message} />
+            </Col>
+          )
         ) : (
           <Col xs={12}>
-            <Asset src={NoResults} message={message} />
+            <Asset spinner />
           </Col>
-        )
-      ) : (
-        <Col xs={12}>
-          <Asset spinner />
-        </Col>
-      )}
-    </Row>
+        )}
+      </Row>
     </Container>
   );
 }
