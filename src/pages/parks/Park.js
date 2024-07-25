ef1d35511e8ab9263350a1a6aead7da4621ca179
Stephen Sure <stephan.sure@gmail.com>
2024-07-25 19:38:26 +0000

    Change position of icons

--- src/pages/parks/Park.js
+++ src/pages/parks/Park.js
@@ -103,79 +103,81 @@ const Park = (props) => {
           </Col>
 
           <Col className="py-2 p-0 p-lg-2 order-lg-1" lg={7}>
-            <div className={styles.DataCard}>
-              <div className={styles.Header}>
-                {name && <h2>{name}</h2>}
-                {user && profile_picture && (
-                  <div className={styles.UserInfo}>
-                    <Link
-                      to={`/profiles/${profile_id}`}
-                      className="d-flex align-items-center"
-                    >
-                      <Avatar src={profile_picture} height={55} />
-                      <span className={styles.UserName}>
-                        Author: {user} 
-                      </span>
-                    </Link>
-                  </div>
+            <div className={styles.ContentWrapper}>
+              <div className={styles.DataCard}>
+                <div className={styles.Header}>
+                  {name && <h2>{name}</h2>}
+                  {user && profile_picture && (
+                    <div className={styles.UserInfo}>
+                      <Link
+                        to={`/profiles/${profile_id}`}
+                        className="d-flex align-items-center"
+                      >
+                        <Avatar src={profile_picture} height={55} />
+                        <span className={styles.UserName}>
+                          Author: {user} 
+                        </span>
+                      </Link>
+                    </div>
+                  )}
+                </div>
+                {description && <p>{description}</p>}
+                {total_number_of_rides && (
+                  <p>Total Number of Rides: {total_number_of_rides}</p>
                 )}
+                {total_number_of_coasters && (
+                  <p>Total Number of Coasters: {total_number_of_coasters}</p>
+                )}
+                {thrill_factor && <p>Thrill Factor: {thrill_factor}</p>}
+                {overall_rating && <p>Overall Rating: {overall_rating}</p>}
+                {website && (
+                  <p>
+                    Website:{" "}
+                    <a href={website} className={styles.Link}>
+                      {website}
+                    </a>
+                  </p>
+                )}
+                {created_at && <p>Created At: {created_at}</p>}
+                {updated_at && <p>Updated At: {updated_at}</p>}
               </div>
-              {description && <p>{description}</p>}
-              {total_number_of_rides && (
-                <p>Total Number of Rides: {total_number_of_rides}</p>
-              )}
-              {total_number_of_coasters && (
-                <p>Total Number of Coasters: {total_number_of_coasters}</p>
-              )}
-              {thrill_factor && <p>Thrill Factor: {thrill_factor}</p>}
-              {overall_rating && <p>Overall Rating: {overall_rating}</p>}
-              {website && (
-                <p>
-                  Website:{" "}
-                  <a href={website} className={styles.Link}>
-                    {website}
-                  </a>
-                </p>
-              )}
-              {created_at && <p>Created At: {created_at}</p>}
-              {updated_at && <p>Updated At: {updated_at}</p>}
+
+              <div className={styles.ParkIcons}>
+                <div className={styles.BucketIconCounter}>
+                  <div className={styles.IconWrapper}>
+                    {bucketlist_id ? (
+                      <div className={`text-center ${styles.SelectedBucket}`} onClick={handleUndoBucketlist}>
+                        <i className={`fas fa-bucket ${styles.Bucket}`} />
+                      </div>
+                    ) : currentUser ? (
+                      <div className={`text-center ${styles.BucketOutline}`} onClick={handleBucketlist}>
+                        <i className={`fas fa-bucket ${styles.BucketOutlineIcon}`} /> 
+                      </div>
+                    ) : (
+                      <OverlayTrigger
+                        placement="top"
+                        overlay={<Tooltip>Log in to add parks to your bucketlist!</Tooltip>}
+                      >
+                        <div className="text-center">
+                          <i className="fas fa-bucket" />
+                        </div>
+                      </OverlayTrigger>
+                    )}
+                    <span>{bucketlist_count}</span>
+                  </div>
+                  <p className={styles.IconLabel}>bucketlist</p>
+                </div>
+                <div className={styles.StarIconCounter}>
+                  <div className={styles.IconWrapper}>
+                    <i className={`far fa-solid fa-star ${styles.star}`}/>
+                    <span>{ratings_count}</span>
+                  </div>
+                  <p className={styles.IconLabel}>ratings</p>
+                </div>
+              </div>
             </div>
           </Col>
         </Row>
-
-        <div className={`${styles.ParkIcons} mt-3 d-flex justify-content-center`}>
-          <div className={styles.BucketIconCounter}>
-            <div className={styles.IconWrapper}>
-              {bucketlist_id ? (
-                <div className={`text-center ${styles.SelectedBucket}`} onClick={handleUndoBucketlist}>
-                  <i className={`fas fa-bucket ${styles.Bucket}`} />
-                </div>
-              ) : currentUser ? (
-                <div className={`text-center ${styles.BucketOutline}`} onClick={handleBucketlist}>
-                  <i className={`fas fa-bucket ${styles.BucketOutlineIcon}`} /> 
-                </div>
-              ) : (
-                <OverlayTrigger
-                  placement="top"
-                  overlay={<Tooltip>Log in to add parks to your bucketlist!</Tooltip>}
-                >
-                  <div className="text-center">
-                    <i className="fas fa-bucket" />
-                  </div>
-                </OverlayTrigger>
-              )}
-              <span>{bucketlist_count}</span>
-            </div>
-            <p className={styles.IconLabel}>bucketlist</p>
-          </div>
-          <div className={styles.StarIconCounter}>
-            <div className={styles.IconWrapper}>
-              <i className={`far fa-solid fa-star ${styles.star}`}/>
-              <span>{ratings_count}</span>
-            </div>
-            <p className={styles.IconLabel}>ratings</p>
-          </div>
-        </div>
 
         {is_owner && parkPage && (
           <div className="mt-3 d-flex justify-content-center align-items-center" id="ParkButtonContainer">
